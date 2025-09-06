79cb729f8d5ffb3b9417ce98f2e8caf97211517e
Javid Ahmed <57677851+javidahmed64592@users.noreply.github.com>
2025-09-05 22:10:23 -0400

    Bump version to 1.0.2 (#25)
    * Added framework for RAG system, though this does not yet contain data which can be used by the LLM
    
    * Updated copy-to-clipboard button in command and script boxes to provide more feedback
    
    * Minor fixes for the Windows installer script
    
    * Updated documentation

--- cyber-query-ai-frontend/src/lib/__tests__/version.test.ts
+++ cyber-query-ai-frontend/src/lib/__tests__/version.test.ts
@@ -2,6 +2,6 @@ import { version } from "../version";
 
 describe("version", () => {
   it("should return the correct version from package.json", () => {
-    expect(version).toBe("1.0.1");
+    expect(version).toBe("1.0.2");
   });
 });
